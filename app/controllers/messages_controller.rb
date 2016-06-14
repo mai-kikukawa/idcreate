ac457021501d48d093d4b95c1c4698e8c0d9dbfa
maikikukawa <mai-kikukawa@dac.co.jp>
2016-06-14 08:47:19 +0000

    grid

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,49 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     # Messageを全て取得する。
     @messages = Message.all
   end
 
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'リクエストを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'リクエストを保存しました'
+    else
+      # リクエストが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "リクエストの保存に失敗しました。"
+      render 'index'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
   private
   def message_params
     params.require(:message).permit(:tipe, :media, :start, :finish, :rink)
   end
-  ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ActiveRecord::Base
+  # 広告種別は必須入力かつ30文字以内
+  validates :media , length: { maximum: 30 } , presence: true
+  # 媒体は必須入力かつ30文字以内
+  validates :media , length: { maximum: 30 } , presence: true
+  # リンク先URLは必須入力
+  validates :rink , presence: true
 end

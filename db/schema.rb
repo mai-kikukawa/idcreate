7483faf2af5d09e138d93083d0d54fdd48dc78d3
maikikukawa <mai-kikukawa@dac.co.jp>
2016-06-14 09:35:43 +0000

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608074454) do
+ActiveRecord::Schema.define(version: 20160614093408) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "tipe"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20160608074454) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
